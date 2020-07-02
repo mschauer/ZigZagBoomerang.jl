e4aad9be73e89a7d2d606ce0b4c7f2fad0229ba3
Moritz Schauer <moritzschauer@web.de>
2020-07-02 22:37:34 +0200

    Implement mean of reference measure for Boomerang (#2)

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,17 +12,14 @@ Random.seed!(1)
 # gradient of ϕ(x)
 ∇ϕ(x) = x - π
 
-
 x0, θ0 = 0.01, 1.0
 T = 5000.0
 out1 = ZigZagBoomerang.pdmp(∇ϕ, x0, θ0, T, 10.0, ZigZag())
-out2 = ZigZagBoomerang.pdmp(∇ϕ, x0, θ0, T, 4.0, Boomerang(0.5))
-
-
+B = Boomerang(2.0, 0.5)
+out2 = ZigZagBoomerang.pdmp(∇ϕ, x0, θ0, T, 4.0, B)
 
 @testset "ZigZag" begin
     @test T/10 < length(out1) < T*10
-
     est = 1/T*sum((eventposition.(out1)[1:end-1] + eventposition.(out1)[2:end])/2 .* diff(eventtime.(out1)))
     @test abs(est-pi) < 2/sqrt(length(out1))
     dt = 0.01
@@ -34,9 +31,8 @@ end
 
 @testset "Boomerang" begin
     @test T/10 < length(out2) < T*10
-
     dt = 0.01
-    traj = ZigZagBoomerang.discretization(out2, Boomerang(NaN), dt)
+    traj = ZigZagBoomerang.discretization(out2, B, dt)
     est = mean(traj.x)
     @test abs(est-pi) < 2/sqrt(length(out2))
 end
