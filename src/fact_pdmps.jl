3edc099bd7e289c1b513425fdd40150aca5bca08
SebaGraz <sebastiano.grazzi@gmail.com>
2020-07-04 11:44:49 +0200

    try to group implementations with common features

---
+++ src/fact_pdmps.jl
@@ -0,0 +1,190 @@
+using DataStructures
+using Statistics
+using SparseArrays
+using LinearAlgebra
+
+"""
+    struct ZigZag(Γ, μ) <: ContinuousDynamics
+
+Flag for local implementation of the ZigZag which exploits
+any conditional independence structure of the target measure,
+in form the argument Γ, a sparse precision matrix approximating
+target precision. μ is the approximate target mean.
+"""
+struct ZigZag{T,S} <: ContinuousDynamics
+    Γ::T
+    μ::S
+end
+
+"""
+    FactBoomerang(μ, λ) <: ContinuousDynamics
+
+Dynamics preserving the N(μ, 1) measure (Boomerang)
+with refreshment time `λ`
+"""
+struct FactBoomerang{R, T, S} <: ContinuousDynamics
+    Γ::R
+    μ::T
+    λref::S
+end
+FactBoomerang(Γ, λ) = FactBoomerang(Γ, 0.0, λ)
+
+#TODO
+# FactBoomerang(d, λ) = FactBoomerang(fullyconnecetdgraph(d), 0.0, λ)
+
+"""
+    neighbours(G::Vector{<:Pair}, i) = G[i].second
+
+Return extended neighbourhood of `i` including `i`.
+`G`: graphs of neightbourhoods
+"""
+neighbours(G::Vector{<:Pair}, i) = G[i].second
+
+"""
+    move_forward!(τ, t, x, θ, Z::ZigZag)
+Updates the position `x`, velocity `θ` and time `t` of the
+process after a time step equal to `τ` according to the deterministic
+dynamics of the `ZigZag` sampler: (x(τ), θ(τ)) = (x(0) + θ(0)*t, θ(0)).
+`x`: current location, `θ`: current velocity, `t`: current time,
+"""
+move_forward!(τ, t, x, θ, Z::ZigZag) = move_forward!(τ, t, x, θ, Z::Bps)
+
+
+#same as Boomerang
+move_forward!(τ, t, x, θ, Z::FactBoomerang) = move_forward!(τ, t, x, θ, Z::Boomerang)
+
+"""
+        reflect!(i, θ, x, Z)
+Reflection rule of `ZigZag` sampler at reflection time.
+`i`: coordinate which flips sign, `θ`: velocity, `x`: position (not used for
+the `ZigZag`)
+"""
+function reflect!(i, θ, x, Z::ZigZag)
+    θ[i] = -θ[i]
+    θ
+end
+
+# Same as Zig-Zag
+reflect!(i, θ, x, ::FactBoomerang) = reflect!(i, θ, x, ::ZigZag)
+
+
+normsq(x::Real) = abs2(x)
+normsq(x) = dot(x,x)
+"""
+    λ(∇ϕ, i, x, θ, Z::ZigZag)
+`i`th Poisson rate of the `ZigZag` sampler
+"""
+function λ(∇ϕ, i, x, θ, Z::ZigZag)
+    pos(∇ϕ(x, i)*θ[i])
+end
+
+"""
+    λ(∇ϕ, i, x, θ, Z::FactBoomerang)
+`i`th Poisson rate of the `FactBoomerang` sampler
+"""
+function λ(∇ϕ, i, x, θ, Z::FactBoomerang)
+    pos((∇ϕ(x, i) - (x[i] - B.μ[i]))*θ[i])
+end
+
+
+"""
+    ab(G, i, x, θ, c, Z::ZigZag)
+
+Returns the constant term `a` and linear term `b` when computing the Poisson times
+from the upper upper bounding rates λᵢ(t) = max(a + b*t)^2. The factors `a` and `b`
+can be function of the current position `x`, velocity `θ`, tuning parameter `c` and
+the Graph `G`
+"""
+function ab(G, i, x, θ, c, Z::ZigZag)
+    a = c[i] + θ[i]*(dot(Z.Γ[:, i], x)  - dot(Z.Γ[:, i], Z.μ))
+    b = θ[i]*dot(Z.Γ[:, i], θ)
+    a, b
+end
+
+
+"""
+    λ_bar(G, i, x, θ, c, Z::ZigZag)
+
+Computes the bounding rate `λ_bar` at position `x` and velocity `θ`.
+"""
+λ_bar(G, i, x, θ, c, Z::ZigZag) = pos(ab(G, i, x, θ, c, Z::ZigZag)[1])
+event(i, t, x, θ, Z::ZigZag) = (i, t, x[i])
+event(i, t, x, θ, Z::FactBoomerang) = (i, t, x[i], θ[i])
+
+
+
+
+"""
+    pdmp_inner!(Ξ, G, ∇ϕ, x, θ, Q, t, c, (num, acc), Z::ZigZag;
+        factor=1.5, adapt=false)
+Inner loop of the `ZigZag` algorithm.
+Input: a dependency graph `G`, gradient `∇ϕ`,
+current position `x`, velocity `θ`, Queue of events `Q`, time `t`, and tuning parameter `c`.
+
+The sampler 1) extracts from the queue the first event time. 2) moves deterministically
+according to the `ZigZag` dynamics until event time. 3) Evaluates whether the event
+time is a reflection time or not. 4) If it is a reflection time, the velocity reflects
+according the the `ZigZag` reflection rule and updates `Q` according to the
+dependency graph `G`. `(num, acc)` counts how many event times occour and how many of
+those are real reflection times.
+"""
+function pdmp_inner!(Ξ, G, ∇ϕ, x, θ, Q, t, c, (num, acc), Z::ZigZag;
+        factor=1.5, adapt=false)
+
+    i, t′ = dequeue_pair!(Q)
+    if t′ - t < 0
+        error("negative time")
+    end
+    t, x, θ = move_forward!(t′ - t, t, x, θ, Z)
+
+    l, lb = λ(∇ϕ, i, x, θ, Z), λ_bar(G, i, x, θ, c, Z)
+    num += 1
+
+    if rand()*lb < l
+        acc += 1
+        if l >= lb
+            !adapt && error("Tuning parameter `c` too small.")
+            c[i] *= factor
+        end
+        θ = reflect!(i, θ, x, Z)
+        for j in neighbours(G, i)
+            j == i && continue
+            Q[j] = t + poisson_time(ab(G, j, x, θ, c, Z)..., rand())
+        end
+        push!(Ξ, event(i, t, x, θ, Z))
+    end
+    enqueue!(Q, i=>t + poisson_time(ab(G, i, x, θ, c, Z)..., rand()))
+    t, x, θ, (num, acc)
+end
+
+"""
+    pdmp(∇ϕ, t0, x0, θ0, T, c, Z::ZigZag; factor=1.5, adapt=false) = Ξ, (t, x, θ), (acc, num)
+
+`ZigZag` algorithm.
+Input: Gradient of negative log density `∇ϕ`, initial time `t0`,
+initial position `x0`, initial velocity `θ0`, final clock `T`, tuning parameter `c`.
+
+The process moves at to time `T` with invariant mesure μ(dx) ∝ exp(-ϕ(x))dx and outputs
+a collection of reflection points `Ξ` which, together with the initial triple `x`
+`θ` and `t` are sufficient for reconstructuing continuously the continuous path
+"""
+function pdmp(∇ϕ, t0, x0, θ0, T, c, Z::ZigZag; factor=1.5, adapt=false)
+
+    #sparsity graph
+    G = [i => rowvals(Z.Γ)[nzrange(Z.Γ, i)] for i in eachindex(θ0)]
+
+    t, x, θ = t0, copy(x0), copy(θ0)
+    num = acc = 0
+
+    Q = PriorityQueue{Int,Float64}()
+
+    for i in eachindex(θ)
+        enqueue!(Q, i=>poisson_time(ab(G, i, x, θ, c, Z)..., rand()))
+    end
+
+    Ξ = [event(1, t, x, θ, Z)][1:0]
+    while t < T
+        t, x, θ, (num, acc) = pdmp_inner!(Ξ, G, ∇ϕ, x, θ, Q, t, c, (num, acc), Z; factor=1.5)
+    end
+    Ξ, (t, x, θ), (acc, num)
+end
---
+++ src/pdmps.jl
@@ -0,0 +1,130 @@
+"""
+    ContinuousDynamics
+
+Abstract type for the deterministic dynamics of PDMPs
+"""
+abstract type ContinuousDynamics end
+
+eventtime(x) = x[1]
+eventposition(x) = x[2]
+"""
+    Boomerang(μ, λ) <: ContinuousDynamics
+
+Dynamics preserving the N(μ, 1) measure (Boomerang)
+with refreshment time `λ`
+"""
+struct Boomerang{T, S} <: ContinuousDynamics
+    μ::T
+    λref::S
+end
+Boomerang(λ) = Boomerang(0.0, λ)
+
+"""
+    Bps{T} <: ContinuousDynamics
+λref::T : refreshment rate which has to be strivtly positive
+Flag for the Bouncy particle sampler
+"""
+struct Bps{T} <: ContinuousDynamics
+    λref::T
+end
+
+"""
+    move_forward(τ, t, x, θ, ::Bps)
+Updates the position `x`, velocity `θ` and time `t` of the
+process after a time step equal to `τ` according to the deterministic
+dynamics of the Buoncy particle sampler (`Bps`): (x(τ), θ(τ)) = (x(0) + θ(0)*t, θ(0)).
+`x`: current location, `θ`: current velocity, `t`: current time,
+"""
+function move_forward!(τ, t, x, θ, Z::BPS)
+    t += τ
+    x .+= θ .* τ
+    t, x, θ
+end
+
+"""
+    move_forward(τ, t, x, θ, B::Boomerang)
+Updates the position `x`, velocity `θ` and time `t` of the
+process after a time step equal to `τ` according to the deterministic
+dynamics of the `Boomerang` sampler which are the Hamiltonian
+dynamics preserving the Gaussian measure:
+: x_t = μ +(x_0 − μ)*cos(t) + v_0*sin(t), v_t = −(x_0 − μ)*sin(t) + v_0*cos(t)
+`x`: current location, `θ`: current velocity, `t`: current time.
+"""
+function move_forward(τ, t, x, θ, B::Boomerang)
+    x_new = (x .- B.μ)*cos(τ) .+ θ*sin(τ) .+ B.μ
+    θ .= -(x .- B.μ)*sin(τ) .+ θ*cos(τ)
+    t + τ, x_new, θ
+end
+
+
+#Poisson rates which determine the first reflection time
+λ(∇ϕ, x, θ, F::Bps) = max(0.0, dot(θ, ∇ϕ(x)))
+λ(∇ϕ, x, θ, B::Boomerang) = max(0.0, dot(θ, ∇ϕ(x) .- (x .- B.μ))
+
+# affine bounds for Bps
+λ_bar(x, θ, c, ::Bps) = max(0.0, c + dot(θ,x))
+
+
+# constant bound for Boomerang with global bounded |∇ϕ(x)|
+# suppose |∇ϕ(x, :Boomerang)| ≤ C. Then λ(x(t),θ(t)) ≤ C*sqrt(x(0)^2 + θ(0)^2)
+λ_bar(x, θ, c, B::Boomerang) = sqrt(norm(θ)^2 + norm(x .- B.μ)^2)*c #Global bound
+
+
+# waiting times
+ab(x, θ, c, ::Bps) = (c + dot(θ,x), 1.0)
+ab(x, θ, c, B::Boomerang) = (sqrt(norm(θ)^2 + norm(x - B.μ)^2)*c, 0.0)
+
+# waiting_time
+waiting_time_ref(::Bps) = poisson_time(B.λref, 0.0, rand()
+waiting_time_ref(B::Boomerang) = poisson_time(B.λref, 0.0, rand())
+
+reflect!(∇ϕ, θ, x, ::Bps) = θ - 2*dot(∇ϕ, θ)/dot(∇ϕ,∇ϕ)*∇ϕ
+reflect!(∇ϕ, θ, x, ::Boomerang) = θ - 2*dot(∇ϕ, θ)/dot(∇ϕ,∇ϕ)*∇ϕ
+
+
+# Algorithm for one dimensional pdmp (ZigZag or Boomerang)
+"""
+    pdmp(∇ϕ, x, θ, T, Flow::ContinuousDynamics; adapt=true,  factor=2.0)
+
+Run a piecewise deterministic process from location and velocity `x, θ` until time
+`T`. `c` is a tuning parameter for the upper bound of the Poisson rate.
+If `adapt = false`, `c = c*factor` is tried, otherwise an error is thrown.
+
+Returns vector of tuples `(t, x, θ)` (time, location, velocity) of
+direction change events.
+"""
+function pdmp(∇ϕ, x, θ, T, c, Flow::ContinuousDynamics; adapt=false, factor=2.0)
+    scaleT = Flow isa Boomerang ? 1.25 : 1.0
+    T = T*scaleT
+    t = zero(T)
+    Ξ = [(t, x, θ)]
+    τref = waiting_time_ref(Flow)
+    τ =  poisson_time(ab(x, θ, c, Flow)..., rand())
+    while t<T
+        if τref < τ
+            t, x, θ = move_forward(τref, t, x, θ, Flow)
+            θ = randn()
+            τref = waiting_time_ref(Flow)
+            τ =  poisson_time(ab(x, θ, c, Flow)..., rand())
+            push!(Ξ, (t, x, θ))
+        else
+            t, x, θ = move_forward(τ, t, x, θ, Flow)
+            τref -= τ
+            τ = poisson_time(ab(x, θ, c, Flow)..., rand())
+            l, lb = λ(∇ϕ, x, θ, Flow), λ_bar(x, θ, c, Flow)
+            if rand()*lb < l
+                if l >= lb
+                    !adapt && error("Tuning parameter `c` too small.")
+                    c *= factor
+                end
+                θ = reflect!(θ, x, Flow)
+                push!(Ξ, (t, x, θ))
+            end
+        end
+    end
+    return Ξ
+end
+
+
+include("discretize.jl")
+include("localzigzag.jl")
