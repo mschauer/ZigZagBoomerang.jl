f2695cbdee4cf929a6f26f4a91803c845b4b423a
SebaGraz <sebastiano.grazzi@gmail.com>
2020-07-05 10:56:54 +0200

    clean up. Move 1d boom and zigzag src code

---
+++ src/types.jl
@@ -0,0 +1,24 @@
+"""
+    ContinuousDynamics
+
+Abstract type for the deterministic dynamics of PDMPs
+"""
+abstract type ContinuousDynamics end
+"""
+    ZigZag1d <: ContinuousDynamics
+
+Dynamics preserving the Lebesgue measure (1 dimensional ZigZag sampler)
+"""
+
+struct ZigZag1d <: ContinuousDynamics  end
+"""
+    Boomerang1d(μ, λ) <: ContinuousDynamics
+
+Dynamics preserving the N(μ, 1) measure (Boomerang1d)
+with refreshment time `λ`
+"""
+struct Boomerang1d{T} <: ContinuousDynamics
+    μ::T
+    λref::T
+end
+Boomerang1d(λ) = Boomerang1d(0.0, λ)
