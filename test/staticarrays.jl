68bc3b142fe779dd37395d725aabc6e2f539f0ac
mschauer <moritzschauer@web.de>
2021-03-29 10:47:28 +0200

    Use requires

--- test/staticarrays.jl
+++ test/staticarrays.jl
@@ -1,5 +1,5 @@
-using StaticArrays
 using ZigZagBoomerang
+using StaticArrays
 using LinearAlgebra
 using SparseArrays
 using Random
@@ -31,21 +31,6 @@ using Random
 
     @time trace, (tT, xT, θT), (acc, num) = spdmp(∇ϕ, t0, x0, θ0, T, c, Z, Γ)
     xs = last.(collect(discretize(trace, 0.01)))
-end
-
-function ZigZagBoomerang.move_forward!(τ, t, x::SVector, θ, Z::Union{BouncyParticle, ZigZag})
-    t += τ
-    x += θ .* τ
-    t, x, θ
-end
-ZigZagBoomerang.smove_forward!(τ, t, x::SVector, θ, f, Z::Union{BouncyParticle, ZigZag}) = ZigZagBoomerang.move_forward!(τ, t, x, θ, Z)
-function ZigZagBoomerang.reflect!(∇ϕx, x::SVector, θ, ::Union{BouncyParticle, Boomerang})
-    θ -= (2*dot(∇ϕx, θ)/ZigZagBoomerang.normsq(∇ϕx))*∇ϕx
-    θ
-end
-function ZigZagBoomerang.refresh!(θ::SVector{d}, F) where {d}
-    ρ̄ = sqrt(1-F.ρ^2)
-    F.ρ*θ + ρ̄*@SVector(randn(d))
 end
 
 
