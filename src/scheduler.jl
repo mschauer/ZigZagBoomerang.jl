8984371baa1276b715abe12dd7b34386916dccbb
mschauer <moritzschauer@web.de>
2021-04-25 21:38:41 +0200

    Broken unbroken

--- src/scheduler.jl
+++ src/scheduler.jl
@@ -1,5 +1,9 @@
 using ZigZagBoomerang: SPriorityQueue, enqueue!
 using FunctionWranglers
+using StructArrays
+using StructArrays: components
+
+
 using Random, LinearAlgebra
 
 @inline @generated function _sfindmin(wrangler::FunctionWrangler{TOp, TNext}, tmin, j, n, args...) where {TNext, TOp}
@@ -13,7 +17,7 @@ using Random, LinearAlgebra
 end
 
 """
-    sfindmin(wrangler::FunctionWrangler, args...)
+    sfindmin(wrangler::FunctionWrangler#=, args...=#)
     
 Look for the function which returns smallest value for the given arguments, and returns its index.
 """
@@ -24,8 +28,8 @@ sfindmin(wrangler::FunctionWrangler, args...) = _sfindmin(wrangler, Inf, 0, 1, a
 T = 100.0
 d = 10000
 
-function f1!(i, t′, u, event_type, Q)
-    t, x, θ, m = u
+function f1!(i, t′, u)
+    t, x, θ, m = components(u)
     x[i] += θ[i]*(t′ - t[i]) 
     if rand() < 0.1
         θ[i] = -θ[i]
@@ -33,21 +37,21 @@ function f1!(i, t′, u, event_type, Q)
     t[i] = t′
 end
 
-function f2!(i, t′, u, event_type, Q)
-    t, x, θ, m = u
+function f2!(i, t′, u)
+    t, x, θ, m = components(u)
     x[i] += θ[i]*(t′ - t[i]) 
     @assert norm(x[i]) < 1e-7
     θ[i] = -θ[i]
     t[i] = t′
 end
 
-function next1(i, t′, u)
-    t, x, θ, m = u
-    t[i] + randexp()
+function next1(j, e, i, t′, u)
+    t, x, θ, m = components(u)
+    t[j] + randexp()
 end 
-function next2(i, t′, u) 
-    t, x, θ, m = u
-    θ[i]*x[i] >= 0 ? Inf : t[i] - x[i]/θ[i]
+function next2(j, e, i, t′, u) 
+    t, x, θ, m = components(u)
+    θ[j]*x[j] >= 0 ? Inf : t[j] - x[j]/θ[j]
 end
 
 struct Handler{T1,T2,T3,T4,T5}
@@ -58,53 +62,53 @@ struct Handler{T1,T2,T3,T4,T5}
     T::T5
 end
 function Base.iterate(handler::Handler)
-    d = length(handler.state[2])
-    event_type = zeros(Int, d)
-    Q = queue(zeros(d))
-    u = handler.state
-    iterate(handler, (u, event_type, Q))
+    d = length(handler.state)
+    action = zeros(Int, d) # null events to trigger next event computations
+    Q = SPriorityQueue(zeros(d))
+    u = deepcopy(handler.state)
+    iterate(handler, (u, action, Q))
 end
 Base.IteratorSize(::Handler) = Base.SizeUnknown()
 
-function Base.iterate(handler, (u, event_type, Q))
+function Base.iterate(handler, (u, action, Q))
     f! = handler.f
     next = handler.next
     G = handler.G
-    ev = handle!(u, G, f!, next, event_type, Q)
+    ev = handle!(u, G, f!, next, action, Q)
     ev[1] > handler.T && return nothing
-    ev, (u, event_type, Q)
+    ev, (u, action, Q)
 end
 
-function handle(handler)
-     u = handler.state
+function handle(handler#=, args...=#)
+     u = deepcopy(handler.state)
      G = handler.G
      f! = handler.f
      next = handler.next
-     d = length(handler.state[2])
-     event_type = zeros(Int, d)
-     Q = queue(zeros(d))
+     d = length(handler.state)
+     action = zeros(Int, d)
+     Q = SPriorityQueue(zeros(d))
      while true
-        ev = handle!(u, G, f!, next, event_type, Q)
+        ev = handle!(u, G, f!, next, action, Q#=, args...=#)
         ev[1] > handler.T && return ev
      end
 end
 
-function handle!(u, G, f!, next, event_type, Q)
+function handle!(u, G, f!, next, action, Q#=, args...=#)
     # Who is (i) next, when (t′) and what (j) happens?
     i, t′ = peek(Q)
-    e = event_type[i] 
+    e = action[i] 
     # Trigger state change
     if e > 0 # something? 
-        sindex(f!, e, i, t′, u, event_type, Q)
+        sindex(f!, e, i, t′, u, #=, args...=#)
     end
-    # What happens next now has changed for each j
-    for j in neighbours(G, i)
-        τ, e = sfindmin(next, j, t′, u)
+    # What happens next now has changed for each j, trigger updates
+    for j in affected(G, e, i, t′, u#=, args...=#)
+        τ, e = sfindmin(next, j, e, i, t′, u#=, args...=#)
         Q[j] = τ
-        event_type[j] = e
+        action[j] = e
     end
 
-    (t′, i, u[1][i], e)
+    (t′, i, u[i], e)
 end
 function lastiterate(itr) 
     ϕ  = iterate(itr)
@@ -125,13 +129,18 @@ Random.seed!(1)
 fs = FunctionWrangler((f1!, f2!))
 next = FunctionWrangler((next1, next2))
 G = nothing
-neighbours(::Nothing, i) = i 
-h = Handler(G, fs, next, (zeros(d), zeros(d), ones(d), zeros(Int,d)), T)
-trc = @time lastiterate(h)
-h = Handler(G, fs, next, (zeros(d), zeros(d), ones(d), zeros(Int,d)), T)
+affected(::Nothing, e, i, t′, u) = i 
+u0 = StructArray(t=zeros(d), x=zeros(d), θ=ones(d), m=zeros(Int,d))
+h = Handler(G, fs, next, u0, T)
+l_ = @time lastiterate(h)
+@assert l_[3].x == 44.18692841846383
 l = @time handle(h)
+trc = @time collect(h)
 #@code_warntype handler(zeros(d), T, (f1!, f2!));
 
 #using ProfileView
 
 #ProfileView.@profview handler(zeros(d), 10T);
+
+subtrace = [t for t in trc if t[2] == 1]
+lines(getindex.(subtrace, 1), getfield.(getindex.(subtrace, 3), :x))
