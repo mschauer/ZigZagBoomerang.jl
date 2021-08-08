af22d0cf64c05d59861b58e3c626f13d3a7c3bef
SebaGraz <s.grazzi@tudelft.nl>
2021-08-08 18:44:55 +0200

    initialize branch

---
+++ scripts/epidemiology/virus.jl
@@ -0,0 +1,9 @@
+### coding functions for posterior measures in https://projecteuclid.org/journals/bayesian-analysis/volume-4/issue-3/Bayesian-analysis-for-emerging-infectious-diseases/10.1214/09-BA417.full
+
+### Notation
+# NN := population size
+# I := vector of length NN containing the infection times of individuals (∞ for individuals which are not infected) [parameter]
+# N := same as above for notified [given]
+# R := same as above for removed [given]
+# β(t) := matrix with elements [i,j] describing the infection rate from i to j at time t [contains parameters]
+
