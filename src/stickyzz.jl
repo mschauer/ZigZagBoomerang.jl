fb267c1db9dd75e0b09afa88947c86fd21117a82
Sebastiano Grazzi <s.grazzi@tudelft.nl>
2021-11-26 00:38:17 +0100

    Update stickyzz.jl

--- src/stickyzz.jl
+++ src/stickyzz.jl
@@ -26,8 +26,9 @@ struct StickyFlow{T}
     old::T
 end
 
-struct StickyUpperBounds{TG,TΓ,Tstrong,Tc,Tfact}
-    G::TG
+struct StickyUpperBounds{TG,TG2,TΓ,Tstrong,Tc,Tfact}
+    G1::TG
+    G2::TG2
     Γ::TΓ
     strong::Tstrong
     adapt::Bool
@@ -41,11 +42,12 @@ struct StructuredTarget{TG,T∇ϕ}
     #selfmoving ? 
 end
 
+
 StructuredTarget(Γ::SparseMatrixCSC, ∇ϕ) = StructuredTarget([i => rowvals(Γ)[nzrange(Γ, i)] for i in axes(Γ, 1)], ∇ϕ)
 
 function ab(su::StickyUpperBounds, flow, i, u)
     (t, x, v) = u
-    ab(su.G, i, x, v, su.c, flow.old) #TODO
+    ab(su.G1, i, x, v, su.c, flow.old) #TODO
 end
 
 struct AcceptanceDiagnostics
@@ -78,6 +80,7 @@ function freezing_time(barrier, ui)
     end
 end
 
+
 function stickyzz(u0, target::StructuredTarget, flow::StickyFlow, upper_bounds::StickyUpperBounds, barriers::Vector{<:StickyBarriers}, end_condition)
     # Initialize
     (t0, x0, v0) = u0
@@ -86,7 +89,7 @@ function stickyzz(u0, target::StructuredTarget, flow::StickyFlow, upper_bounds::
     # priority queue
     Q = SPriorityQueue{Int,Float64}()
     # Skeleton
-    Ξ = [] # TODO use trace
+    Ξ = Trace(t′, x0, v0, flow.old) # TODO use trace
     # Diagnostics
     acc = AcceptanceDiagnostics(0, 0)
     ## create bounds ab
@@ -116,12 +119,10 @@ function sticky_main(rng, Q::SPriorityQueue, Ξ, t′, u, b, f, target, flow, up
     (t, x, v) = u
     u_old = (copy(t), x, copy(v))
     while finished(end_condition, t′) 
-        (t, x, v) = u
-        d = length(x)
-       
+        t, x, v = u
         i, t′ = stickyzz_inner!(rng, Q, Ξ, t′, u, u_old, b, f, target, flow, upper_bounds, barriers, acc)
-        t, x, θ = u
-        push!(Ξ, event(i, t, x, θ, flow.old))
+        t, x, v = u
+        push!(Ξ, event(i, t, x, v, flow.old))
     end
     return Ξ
 end
@@ -130,31 +131,76 @@ function stickyzz_inner!(rng, Q, Ξ, t′, u, u_old, b, f, target, flow, upper_b
         t, x, v = u
         t_old, _, v_old = u_old
         i, t′ = peek(Q)
-        
-        
+        G = target.G
+        G1 =  upper_bounds.G1
+        G2 =  upper_bounds.G2
         if f[i] # case 1) to be frozen
-            t_old[i] = t[i]
-            t[i] = t′
+            x[i] = -0*v[i] # a bit dangarous
+            t_old[i] = t[i] = t′
+            v_old[i], v[i] = v[i], 0.0 # stop and save speed
             f[i] = false
             di = dir(geti(u, i))
             Q[i] = t′ - log(rand(rng))/barriers[i].κ[di]
-            return i, t′ 
-        elseif x[i] == 0 && θ[i] == 0 # case 2) was frozen
-            queue_time!(Q, u..., i, b, f, flow.old)
+            if upper_bounds.strong == false
+                t, x, v = ssmove_forward!(G, i, t, x, v, t′, flow.old) 
+                t, x, v = ssmove_forward!(G2, i, t, x, v, t′, flow.old)
+                for j in neighbours(G1, i)
+                    if v[j] != 0 # only non-frozen, especially not i
+                        b[j] = ab(upper_bounds, flow, j, u)
+                        t_old[j] = t[j]
+                        Q = queue_time!(Q, u..., j, b, f, flow.old)
+                    end
+                end
+            end
+            push!(Ξ, event(i, t, x, v, flow.old))
+            return i, t′
+        elseif x[i] == 0 && v[i] == 0 # case 2) was frozen
+            t_old[i] = t[i] = t′ # particle does not move, only time
+            v[i], v_old[i] = v_old[i], 0.0 # unfreeze, restore speed
+            t, x, v = ssmove_forward!(G, i, t, x, v, t′, flow.old) 
+            t, x, v = ssmove_forward!(G2, i, t, x, v, t′, flow.old)
+            for j in neighbours(G1, i)
+                if v[j] != 0 # only non-frozen, including i # check!
+                    b[j] = ab(upper_bounds, flow, j, u)
+                    t_old[j] = t[j]
+                    Q = queue_time!(Q, u..., j, b, f, flow.old)
+                end
+            end
+            push!(Ξ, event(i, t, x, v, flow.old))
             return i, t′ 
         else    # was either a reflection 
-            t_old[i] = t[i]
-            t[i] = t′ 
-            queue_time!(Q, u..., i, b, f, flow.old)
                 #time or an event time from the upper bound  
-            if rand() < 0.5 # was a reflection time 
+            t, x, v = ssmove_forward!(G, i, t, x, v, t′, flow.old) 
+            # ∇ϕi = ∇ϕ_(∇ϕ, t, x, θ, i, t′, F, args...)
+            ∇ϕi = target.∇ϕ(x, i) # To change, why Γ is on upperbounds?            
+            # l, lb = sλ(∇ϕi, i, x, θ, F), sλ̄(b[i], t[i] - t_old[i])
+            l, lb = sλ(∇ϕi, i, x, v, flow.old), sλ̄(b[i], t[i] - t_old[i])
+            # acc.num +=1        
+            if rand()*lb < l # was a reflection time
+                # acc.acc += 1
+                if l > lb
+                    !adapt && error("Tuning parameter `c` too small. l/lb = $(l/lb)")
+                    acc = num = 0
+                    adapt!(c, i, factor)
+                end
+                v = reflect!(i, ∇ϕi, x, v, flow.old) # reflect!
+                t, x, v = ssmove_forward!(G2, i, t, x, v, t′, flow.old)  # neighbours of neightbours \ neighbours
+                for j in neighbours(G1, i)
+                    if v[j] != 0
+                        b[j] = ab(upper_bounds, flow, j, u)
+                        t_old[j] = t[j]
+                        queue_time!(Q, u..., j, b, f, flow.old)
+                    end
+                end
+                push!(Ξ, event(i, t, x, v, flow.old))
                 return i, t′ 
             else # was an event time from upperbound -> nothing happens
+                b[i] = ab(upper_bounds, flow, i, u)
+                t_old[i] = t[i]
+                queue_time!(Q, u..., i, b, f, flow.old)
+                # don't save
                 continue
             end               
         end
-
     end
 end
-
-
