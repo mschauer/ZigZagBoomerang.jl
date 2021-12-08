b5558dae893a22d11cc30658241a49839d33e7bd
mschauer <moritzschauer@web.de>
2021-12-08 11:42:30 +0100

    Nice

--- scripts/chessboard.jl
+++ scripts/chessboard.jl
@@ -5,8 +5,8 @@ using SparseArrays
 d = 10
 x = randn(d)
 ∇x = randn(d)
-const σa = 1.0
-const σb = 1.0
+const σa = 2.0
+const σb = 0.5
 # ϕ(x) = 0.5sum(-(x[i]/σa)^2  - ((2.0 - x[i] + x[mod1(i-1, length(x))])/σb)^2 for i in 1:length(x))
 
 ϕ(x) = 0.5sum(-(x[i]/σa)^2 for i in 1:length(x)) + 0.5*sum( -((0.0 - x[i] + x[i+1])/σb)^2 for i in 1:length(x)-1)
@@ -25,18 +25,20 @@ c1 = [(i == 1 || i == d-1) ? -1 : -1 for i in 1:d-1]
 ci = 2.0
 ϕ(x) = 0.5sum(-(x[i]/σa)^2 for i in 1:length(x)) + 0.5*sum( -((ci - x[i] + x[i+1])/σb)^2 for i in 1:length(x)-1)
 #only for $\sigmaa = Inf$
-μ = -inv(Γ)*R'*(ones(d-1).*ci)
+μ = -inv(Γ)*R'/σb^2*(ones(d-1).*ci)
 x = rand(d)
-c = exp(ϕ(x))/exp(-0.5*(x - μ)'*Γ*(x - μ)) 
+c = ϕ(x) + 0.5*(x - μ)'*Γ*(x - μ)
 x = rand(d)
-@test exp(ϕ(x)) ≈ exp(-0.5*(x - μ)'*Γ*(x - μ))*c
-"ok"  
+@test (ϕ(x)) ≈ (-0.5*(x - μ)'*Γ*(x - μ)) + c
+
+
+
 sΓ = sparse(Γ)
 T = 10000.0
 Z = ZigZag(sΓ, μ) 
-∇ϕ(x, i, Γ, μ) = dot(Γ[:,i],(x - μ))
+∇ϕ(x, i, Γ, μ) = ZigZagBoomerang.idot(Γ, i, x) -  ZigZagBoomerang.idot(Γ, i, μ)
 # prior w = 0.5
-wi = 0.2
+wi = 0.05
 ki = 1/(sqrt(2*π*σa^2))/(1/wi - 1)
 println("k equal to $(ki)")
 x0 = rand(d)
@@ -49,20 +51,26 @@ ZigZagBoomerang.sspdmp2(∇ϕ, t0, x0, θ0, T, c, nothing, Z, κ, Γ, μ)
 
 println("sticky Zig-Zag")
 # timer inside sspdmp2
-x0 = fill(10.0, d)
+x0 = fill(5.0, d)
 trace, acc = ZigZagBoomerang.sspdmp2(∇ϕ, t0, x0, θ0, T, c, nothing, Z, κ, Γ, μ)
 ts, xs = ZigZagBoomerang.sep(collect(trace))
 tsh, xsh = ZigZagBoomerang.sep(collect(discretize(trace, 1.0)))
-traceh = [xs2b[i][j] for i in 1:length(xs2b), j in 1:d]
+traceh = [xsh[i][j] for i in 1:length(xsh), j in 1:d]
 
 
 
 using GLMakie
 produce_heatmap = true
 if produce_heatmap
+    burn = 100
     fig1 = Figure()
-    ax = [Axis(fig1[1, j]) for j in 1:1]
-    heatmap!(ax[1], traceh[100:end,:])
+    ax = [Axis(fig1[1, j]) for j in 1:3]
+    heatmap!(ax[1], traceh[burn:end,:])
+    i = 5
+    lines!(ax[2], getindex.(xs, i)[burn:end],getindex.(xs, i+1)[burn:end])
+
+    lines!(ax[3], ts[burn:end], getindex.(xs, i)[burn:end])
+    lines!(ax[3], ts[burn:end], getindex.(xs, i+1)[burn:end])
+    fig1
 end
-fig1
 
