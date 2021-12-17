80084a7c1fe6724ceaa2398156d3654ac2a09d1c
SebaGraz <s.grazzi@tudelft.nl>
2021-12-17 11:45:09 +0100

    fix

--- research/sticky/logistic/logistc_data.jl
+++ research/sticky/logistic/logistc_data.jl
@@ -4,7 +4,7 @@ file = matopen("Prostate_GE.mat")
 data = read(file)
 # WARNING: everything seems to be shift by 1
 y = data["Y"][:] .- 1.0 # rescale by 1
-A = (data["X"])' .- 1.0 # rescale by 1
+A = (data["X"])  .- 1.0 # rescale by 1
 sA = sparse(A)
 sA
 n, p = size(A)
