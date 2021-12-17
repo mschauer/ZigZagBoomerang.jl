536e9864fa82b7fb8d7a0075d620ba2bd8e8b5b8
SebaGraz <s.grazzi@tudelft.nl>
2021-12-17 11:36:29 +0100

    try with real data

---
+++ research/sticky/logistic/logistc_data.jl
@@ -0,0 +1,13 @@
+# FILE DOWNLOADED FROM: https://jundongl.github.io/scikit-feature/datasets.html
+using MAT, SparseArrays
+file = matopen("Prostate_GE.mat")
+data = read(file)
+# WARNING: everything seems to be shift by 1
+y = data["Y"][:] .- 1.0 # rescale by 1
+A = (data["X"])' .- 1.0 # rescale by 1
+sA = sparse(A)
+sA
+n, p = size(A)
+@show n, p
+sΓ = sA'*sA
+@show sparsity(sA), sparsity(sΓ)
