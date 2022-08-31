a35a580c28d877352e54570139eaaa197599d9f9
mschauer <moritzschauer@web.de>
2022-08-31 17:26:28 +0200

    Test queues

--- test/priority.jl
+++ test/priority.jl
@@ -1,5 +1,10 @@
 using DataStructures
 using ZigZagBoomerang: LinearQueue, PriorityQueues, rcat
+using ZigZagBoomerang, Graphs
+using ZigZagBoomerang: PartialQueue, dequeue!, saturate, rkey, div1
+using Graphs: Edge
+using Random
+using Base.Threads
 
 @testset "Queues" begin
     L = LinearQueue(0:2, [3.0, 1.0, 0.5])
@@ -31,4 +36,53 @@ using Test
     Q = PartialQueue(G, vals)
 
     Q[2] = 3
+
+
+
+
+    Random.seed!(1)
+
+    # number of regions
+    nregions = 2
+    # number of coordinates/keys
+    d = 10
+
+    # time surface
+    times = rand(d)
+
+    # undirected graph of neighbours
+    G = Graph(Edge.(([i=>i+1 for i in 1:d-1])))
+
+    # Make a Queue-structure keeping track of local minima
+    # (a coordinate is a local minima if it's time is smaller than all neighbours' (via the graph) times)
+    Q = PartialQueue(G, times, nregions)
+
+    # key 1:5 is in region 1, keys 6:10 in region 2
+    # we can work on different regions in parallel... see below
+    @test rkey(Q,5) == 1 
+    @test rkey(Q,6) == 2 
+
+
+    # dequeue! some local minima to work with
+    minima = dequeue!(Q) 
+
+    @test peek(Q) == [[],[]] # currently all local minima are removed from the queue to be worked on
+
+    # update/increment local time, (in parallel thanks to the threads makro)
+    for r in 1:nregions
+        for (i,t) in minima[r]
+            println("work with $i, $t on $(Threads.threadid())")
+            Q[i] = t + rand()
+        end
+    end
+    
+    # internal check
+    @test try 
+            ZigZagBoomerang.checkqueue(Q, fullcheck=true) == nothing
+          catch 
+            false 
+          end  
+
+    # By now we have a new set of local minimas we can handle in threads 
+    @test peek(Q) != [[],[]]
 end
